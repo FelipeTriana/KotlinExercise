0058d760ece001423da53ed51cb2fd3abc42018e
FelipeTriana <felipe.trianag@udea.edu.co>
2022-05-03 00:43:06 -0500

    PrintSalary view added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,22 +2,7 @@ import java.util.*
 
 fun main(args: Array<String>) {
 
-    val manager = Manager()
-    val operator = Operator(hoursWorked = 230)
-    val accountant = Accountant()
-
-    println("Escriba el número del rol(1: Manager, 2:Operador, 3:Contador) despues pulse enter")
-    val scan = Scanner(System.`in`)
-    val rol = scan.nextLine()
-
-    val  salary = Salary()
-    val result: String
-    when (rol) {
-        "1" -> result = salary.calculateSalary(manager)
-        "2" -> result = salary.calculateSalary(operator)
-        "3" -> result = salary.calculateSalary(accountant)
-        else -> result = "El numero ingresado no corresponde a ningun rol"
-    }
-    println(result)
+    val salary = PrintSalary()
+    salary.printSalary()
 
 }
